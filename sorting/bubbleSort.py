ef1798ecd3c662bc7a3234c254079ceb361364d3
LunaLau0 <yiyangliu@keyst.one>
2021-07-23 16:05:32 +0800

    add sorting directory and bubbleSort file

---
+++ sorting/bubbleSort.py
@@ -0,0 +1,7 @@
+'''
+Logic:
+1. i = 0, n = length(List)
+2. compare List[i] & List[i+1]. The bigger, the later.
+3. i++ & repeat 2., until i = n-1. End with List[i+1] == max(List).
+4. call List[0:n-1]. repeat 1-3, until n == 1.
+'''
